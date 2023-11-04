0b8e03aa3db346870098a369fb78f20438fdc026
Linus Warecka <linus.warecka@mailbox.org>
2023-11-04 23:45:15 +0100

    gute nacht

--- ratbagInterface.js
+++ ratbagInterface.js
@@ -23,7 +23,22 @@ export class RatbagInterface {
         this.device_list = await this.get_device_list();
     }
     async get_device_list() {
+        let flags = Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE | Gio.SubprocessFlags.STDIN_PIPE;
+        let proc = Gio.Subprocess.new(
+            ["ratbagctl", "list"], flags
+        )
+        let [text, stderr_] = await proc.communicate_utf8_async(null, null);
+        text = text.split("\n");
+        text = text.map((str) => (str.split(":"))[0]).slice(0,-1);
+
+
+        console.log("Detected ratbagctl version: " + text);
+        console.log(text);
+        return null
 
     }
 
 }
+
+
+class RatbagDevice {}
